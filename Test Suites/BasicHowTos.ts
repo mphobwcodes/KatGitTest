e5c94130f28bfed2f150b067c9eecd9389dd2b75
mphobwcodes <mpho@mathcraft.com>
2019-01-31 12:28:40 -0500

    Reporting

---
+++ Test Suites/BasicHowTos.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BasicHowTos</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2019318d-fbc4-400e-a68f-63bb8a62be29</testSuiteGuid>
+   <testCaseLink>
+      <guid>c32eacbf-8723-49ae-8669-32aedc4c7125</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add A Record</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d5c0f4f-df87-41a3-b19c-c501f71063e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DeleteARecord</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fdebf9ef-6014-4225-afe0-1a2ca61006d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Update An Exisiting Training</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf00340f-5b47-4fb9-89ef-e54dcb4e7200</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/View a Record</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
